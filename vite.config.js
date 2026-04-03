d5ba3df64b2d02ebb82eb9882bc690d5ace0d8b1
junior barreto <juniorbarreto562@gmail.com>
2026-04-03 16:17:24 -0300

    fix: update vite config for MPA and remove vercel.json greedy rewrite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   build: {
-    outDir: 'dist'
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        lobby: resolve(__dirname, 'lobby.html'),
+        master: resolve(__dirname, 'master.html'),
+        player: resolve(__dirname, 'player.html')
+      }
+    }
   }
 })
